28823caaa14bb6f80b9b70a2e9f275ff21b330af
bijucksae <jiwonmin000@gmail.com>
2024-11-14 20:46:47 +0900

    시스템에서 파티클 관리

--- FireSystems.js
+++ FireSystems.js
@@ -1,12 +1,30 @@
-class ParticleSystem {
+class FireSystem {
   constructor(position) {
       this.origin = position.copy();
       this.particles = [];
+      this.subsytems = [];
   }
 
   addParticle() {
+    if(this.particles.length < maxParticles){
       this.particles.push(new Particle(this.origin));
   }
+}
+
+addSubsystem(subsytem){
+  this.subsytems.push(subsytem);
+}
+
+run() {
+  for (let i = this.particles.length-1; i >= 0; i--) {
+      let p = this.particles[i];
+      p.update();
+      p.display();
+      
+      if (p.isDead()) {
+        this.particles.splice(i, 1);
+      }
+    }
 
   applyGravity(g) {
      this.applyForce(g);
@@ -17,13 +35,5 @@ class ParticleSystem {
       }
   }
 
-  run() {
-      for (let i = this.particles.length-1; i >= 0; i--) {
-          let p = this.particles[i];
-          p.run();
-          if (p.isDead()) {
-            this.particles.splice(i, 1);
-          }
-        }
   }
 }
